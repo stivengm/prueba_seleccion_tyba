ef7828d0a2b8c306b08713270b4c157d0662dcc9
Stiven Gomez <stiven.gomez@nuvu.cc>
2022-03-29 19:09:40 -0500

    Botón primario.

---
+++ lib/gui/widgets/primary_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class PrimaryButton extends StatelessWidget {
+  final String text;
+  final GestureTapCallback? onPressed;
+  const PrimaryButton({ Key? key, required this.text, required this.onPressed }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: TextButton(
+        child: Text(text, style: Theme.of(context).textTheme.titleLarge!.copyWith(color: Colors.white)),
+        style: ButtonStyle(
+          padding: MaterialStateProperty.all<EdgeInsets>(const EdgeInsets.all(12.0)),
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+          backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(50.0),
+              side: const BorderSide(color: Colors.white)
+            )
+          )
+        ),
+        onPressed: onPressed,
+      ),
+    );
+  }
+}
