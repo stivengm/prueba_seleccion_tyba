6bf06ff54c33e5dc807038485582c73b3679d8d9
Stiven Gomez <stiven.gomez@nuvu.cc>
2022-03-29 19:09:30 -0500

    login view componente.

--- lib/gui/views/login_view/login_view.dart
+++ lib/gui/views/login_view/login_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:prueba_seleccion_tyba/gui/views/login_view/form_login_view.dart';
+import 'package:prueba_seleccion_tyba/gui/widgets/primary_button.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({ Key? key }) : super(key: key);
@@ -11,11 +13,25 @@ class LoginView extends StatelessWidget {
   }
 
   Widget _body() {
-    return Column(
-      children: [
-        
-      ],
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const FormLoginView(),
+              const SizedBox(height: 20.0),
+              PrimaryButton(text: 'Login', onPressed: () {})
+            ]
+          ),
+        ),
+      ),
     );
   }
-
 }
