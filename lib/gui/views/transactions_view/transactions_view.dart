d28211c37b3be2a41e899f50aa6f43ab467cfd2c
Stiven Gomez <stiven.gomez@nuvu.cc>
2022-03-29 19:46:31 -0500

    Ajuste de componente transacción.

--- lib/gui/views/transactions_view/transactions_view.dart
+++ lib/gui/views/transactions_view/transactions_view.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
 
 class TransactionsView extends StatelessWidget {
   const TransactionsView({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Transacciones', style: Theme.of(context).textTheme.headline6!.copyWith(color: Colors.white),),
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () => ZoomDrawer.of(context)!.toggle(),
+        ),
+      ),
     );
   }
 }
